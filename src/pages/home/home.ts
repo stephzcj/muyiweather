caea405cd6f894969ca849550def66a9d20b6169
StephZcj <stephzcj@aliyun.com>
2017-05-05 17:31:27 +0800

    添加前后端交互

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,22 +2,27 @@ import { Component ,OnInit} from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Http, Response,Jsonp,Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import {Onedaycast} from './onedaycast';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage implements OnInit{
-  
+  public aaa:any;
   forecasts:Onedaycast[];
+  res:any;
   city:String;
-  constructor(public navCtrl: NavController,private http:Http,private jsonp:Jsonp) {
+  constructor(private jsonp:Jsonp) {
       }
   ngOnInit(): void {
     this.forecasts=this.getForecasts();
     this.city="城市";
-  }
+    this.jsonp.get("http://localhost:8080/?callback=JSONP_CALLBACK").map(res => res.json())
+  .subscribe(data => console.log(data));
+    
+}
+
 
   getForecasts():Onedaycast[]{
     let onedaycast:Onedaycast[]=[
